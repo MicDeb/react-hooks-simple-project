59a62b3ed0dc732febfea0a58839a6e1fb8bf592
Michał Dębek <michal.debek@silksh.pl>
2020-06-17 16:43:25 +0200

    added searching ingredients and remove unnecessary fetching data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -7,20 +7,8 @@ import IngredientList from "./IngredientList";
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
 
-  useEffect(() => {
-    fetch('https://react-hooks-ingredint-list.firebaseio.com/ingredients.json')
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
-          });
-        }
-        setIngredients(loadedIngredients);
-      })
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
+    setIngredients(filteredIngredients);
   }, [])
 
   const addIngredientHandler = (ingredient) => {
@@ -40,8 +28,7 @@ const Ingredients = () => {
 
   const removeIngredientHandler = (id) => {
     setIngredients((state) => {
-      const filteredList = state.filter((elem) => elem.id !== id)
-      return filteredList;
+      return state.filter((elem) => elem.id !== id);
       });
   };
 
@@ -52,7 +39,8 @@ const Ingredients = () => {
       />
 
       <section>
-        <Search />
+        <Search
+          onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList
           ingredients={ingredients}
           onRemoveItem={removeIngredientHandler}
